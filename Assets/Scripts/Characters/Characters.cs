b9e30311bcbd3a1fc09eeb8673da3bcaba86ac30
August590 <45698699+August590@users.noreply.github.com>
2021-05-30 12:44:34 -0500

    Added Character Script and Prefabs
    Character prefabs and scripts added  to be edited in the custom editor.

---
+++ Assets/Scripts/Characters/Characters.cs
@@ -0,0 +1,85 @@
+using UnityEngine;
+public class Characters : MonoBehaviour
+{
+    [SerializeField] private string characterName;
+    [SerializeField] private string ign;
+    [SerializeField] private int age;
+    [SerializeField] private Sprite icon;
+    [SerializeField] private string bio;
+    [SerializeField] private string likes;
+    [SerializeField] private string dislikes;
+    [SerializeField] private string favGames;
+
+    public string getCharacterName()
+    {
+        return characterName;
+    }
+    public void setCharacterName(string characterName)
+    {
+        this.characterName = characterName;
+    }
+
+    public string getIgn()
+    {
+        return ign;
+    }
+    public void setIgn(string ign)
+    {
+        this.ign = ign;
+    }
+
+    public int getAge()
+    {
+        return age;
+    }
+    public void setAge(int age)
+    {
+        this.age = age;
+    }
+
+    public Sprite getIcon()
+    {
+        return icon;
+    }
+    public void setIcon(Sprite icon)
+    {
+        this.icon = icon;
+    }
+
+    public string getBio()
+    {
+        return bio;
+    }
+    public void setBio(string bio)
+    {
+        this.bio = bio;
+    }
+
+    public string getLikes()
+    {
+        return likes;
+    }
+    public void setLikes(string likes)
+    {
+        this.likes = likes;
+    }
+
+    public string getDislikes()
+    {
+        return dislikes;
+    }
+    public void setDislikes(string dislikes)
+    {
+        this.dislikes = dislikes;
+    }
+
+    public string getFavGames()
+    {
+        return favGames;
+    }
+    public void setFavGames(string favGames)
+    {
+        this.favGames = favGames;
+    }
+
+}
